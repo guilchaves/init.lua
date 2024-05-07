bc1d9132019f98428cf2bfde7d9f9b1a11f65beb
guilherme chaves <gchaves.guilherme@gmail.com>
2024-05-07 13:51:26 -0300

    chore: add gofmt

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,20 +1,21 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.eslint,
-				null_ls.builtins.diagnostics.eslint,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.rubocop,
-				null_ls.builtins.diagnostics.rubocop,
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettierd,
+                null_ls.builtins.formatting.eslint,
+                null_ls.builtins.diagnostics.eslint,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.rubocop,
+                null_ls.builtins.diagnostics.rubocop,
                 null_ls.builtins.formatting.gofmt,
                 null_ls.builtins.formatting.goimports_reviser,
-			},
-		})
-	end,
+            },
+        })
+    end,
 }

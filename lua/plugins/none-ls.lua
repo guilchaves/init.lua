dfb8ec57608abd8dbb9554b834d3594e4dbe8805
guilherme chaves <gchaves.guilherme@gmail.com>
2024-05-07 13:19:06 -0300

    chore: add rubocop config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,11 +6,12 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.prettierd,
                 null_ls.builtins.formatting.eslint,
+                null_ls.builtins.diagnostics.eslint,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
-                null_ls.builtins.diagnostics.eslint,
+                null_ls.builtins.formatting.rubocop,
+                null_ls.builtins.diagnostics.rubocop,
             },
         })
 

c75e443730743633baca7c28f3d5674486f204d9
guilherme chaves <gchaves.guilherme@gmail.com>
2024-05-24 15:53:59 -0300

    feat: show filename and path on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,10 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "iceberg_dark",
-                icons_enabled = true,
+				icons_enabled = true,
+			},
+			sections = {
+				lualine_c = { { "filename", path = 1 } },
 			},
 			dependencies = { "nvim-tree/nvim-web-devicons" },
 		})

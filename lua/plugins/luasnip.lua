68a9aa06cf76dc7e10c578284cc604bb3f6ad55b
guilherme chaves <guilherme.chaves@simplasolucoes.com.br>
2024-09-24 16:46:11 -0300

    feat: add vue snippet

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,28 @@
+return {
+	"L3MON4D3/LuaSnip",
+	dependencies = {
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		local ls = require("luasnip")
+		local s = ls.snippet
+		local t = ls.text_node
+		local f = ls.function_node
+
+		local function get_greeting_with_filename()
+            local filename = vim.fn.expand("%:t:r")
+            return "Hello " .. filename
+		end
+
+		ls.add_snippets("vue", {
+			s("vcomp", {
+				t({ "<script setup></script>", "", "<template>", "  <div>" }),
+				f(function()
+					return get_greeting_with_filename()
+				end),
+				t({ "</div>", "</template>", "", "<style scoped>", "", "</style>" }),
+			}),
+		})
+	end,
+}

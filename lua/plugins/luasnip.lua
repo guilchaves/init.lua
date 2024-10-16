71da5c6eb38393e82bda9e0ab822de2d7aa3508a
guilherme chaves <guilherme.chaves@simplasolucoes.com.br>
2024-10-16 17:07:18 -0300

    feat: add vue macro

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -11,9 +11,33 @@ return {
 		local f = ls.function_node
 
 		local function get_greeting_with_filename()
-            local filename = vim.fn.expand("%:t:r")
-            return "Hello " .. filename
+			local filename = vim.fn.expand("%:t:r")
+			return "Hello " .. filename
 		end
+
+		ls.add_snippets("vue", {
+			s("model", {
+				t({"<script setup></script>",
+					"",
+					"<template>",
+                    "  <div>",
+					"    <header>",
+					"      this is my header",
+					"    </header>",
+					"    <main>",
+					"      this is my main",
+                    "      <section>",
+                    "        this is my section",
+                    "      </section>",
+					"    </main>",
+					"    <footer>",
+					"      this is my footer",
+					"    </footer>",
+                    "  </div>",
+					"</template>",
+				}),
+			}),
+		})
 
 		ls.add_snippets("vue", {
 			s("vcomp", {
